938d11be444e1ae62b18567cf69c5d47d3e5229f
induwara20014 <lakshaninduwara067@gmail.com>
2025-08-25 21:49:05 +0530

    Varibles

--- python/variables.py
+++ python/variables.py
@@ -1,4 +1,20 @@
 name = 'kamal'
 age = 20
+weight = 56.2
+
+fruit,animal,vegitable = 'banana','dog','pumkin'
+
+print(fruit)
+print(animal)
+print(vegitable)
+
+print(weight)
 print(age)
 print(name)
+
+mango =  apple = orange = 50
+
+
+print(mango)
+print(apple)
+print(orange)
