4cb9095ea0cc40a9028584f83a0d20ce481f76e9
induwara20014 <lakshaninduwara067@gmail.com>
2025-08-25 22:10:31 +0530

    Varibles

--- python/variables.py
+++ python/variables.py
@@ -14,7 +14,25 @@ print(name)
 
 mango =  apple = orange = 50
 
-
 print(mango)
 print(apple)
 print(orange)
+
+#variables condition
+
+pric45e = 30
+#1price = 30  X
+price1 = 30
+new_price = 30
+_new_price = 30
+#new price = 30  X
+
+Price = 30
+price=70
+priCe = 60
+
+print(price)
+print(Price)
+print(priCe)
+
+#no use to keyword for the variables
