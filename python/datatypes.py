bf1b04d1aed9195f1c397d7f14737ca4e797068c
induwara20014 <lakshaninduwara067@gmail.com>
2025-08-26 21:47:16 +0530

    datatypes

---
+++ python/datatypes.py
@@ -0,0 +1,39 @@
+#Integer
+
+a = 10
+b = -25
+c = 345589986509685685695
+
+print(a)
+print(type(a))
+print(b)
+print(c.__sizeof__())
+
+#float
+
+a = 11.5
+b = -2.4
+
+print(a)
+print(type(a))
+
+#bool
+
+a = True
+b = False
+
+print(a)
+print(type(a))
+
+#comlex
+
+a = 5 + 6j
+b = complex(7,6.5)
+
+print(a)
+print(type(a))
+print(a.real)
+print(a.imag)
+print(b)
+
+
