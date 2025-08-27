a0e0e0696ec0f0ac2d3ebb0d838f8bff9f27ca5c
induwara20014 <lakshaninduwara067@gmail.com>
2025-08-27 17:36:46 +0530

    Arithmetic operators

---
+++ python/Arithmetic operators.py
@@ -0,0 +1,33 @@
+a = 11
+b = 3
+
+#addiction
+c = a + b
+print(c)
+
+#subtraction
+c = a - b
+print(c)
+
+#multiplication
+c = a * b
+print(c)
+
+#division
+c = a/b
+print(c)
+
+#floor division
+c = a//b
+print(c)
+
+#modules
+c = a % b
+print(c)
+
+#power
+c = a ** b
+print(c)
+
+c = (5 % 4) * 2 + (6 // 4)
+print(c)
