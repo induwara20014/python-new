3c545f9f0e24d1fce67c4465912efe6e28f2785b
induwara20014 <lakshaninduwara067@gmail.com>
2025-08-28 22:39:37 +0530

    identity operators

---
+++ python/identity operators.py
@@ -0,0 +1,20 @@
+x = 10
+y = 10
+z = 8
+
+print(x is y)
+print(x is z)
+print(x is not y)
+print(x is not z)
+
+print(id(x))
+print(id(y))
+print(id(z))
+print(x == y)
+
+
+r = [2,4,6]
+b = [2,4,6]
+
+print(id(r))
+print(id(r))
