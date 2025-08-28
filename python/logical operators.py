816742804109f0596dc9b6af6b6896e29fa5e852
induwara20014 <lakshaninduwara067@gmail.com>
2025-08-28 22:23:21 +0530

    Logical operators

---
+++ python/logical operators.py
@@ -0,0 +1,29 @@
+print(True and True)
+print(True and False)
+print(False and False)
+
+print("-----------------------------------------")
+
+print(True or True)
+print(True or False)
+print(False and False)
+
+print("-------------------------------------------")
+
+print(not True)
+print(not False)
+
+print("---------------------------------------------")
+
+print(5>6) and (5>9)
+print(8>4) and (9<3)
+print(7>3) or (6<9)
+
+print("---------------------------------------------")
+
+maths_marks = 67
+science_marks = 43
+Attendance = 54
+
+print(maths_marks > 50 or science_marks > 50)
+print(maths_marks > 50 and science_marks > 50 and Attendance > 80)
