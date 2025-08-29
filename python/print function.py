966413657d0a2119ec3ab75c699dbe0f57be8898
induwara20014 <lakshaninduwara067@gmail.com>
2025-08-29 22:42:25 +0530

    print function

--- python/print function.py
+++ python/print function.py
@@ -2,4 +2,13 @@ print("Hello\nWorld")
 print("-----------------------------------")
 print("Hello\rWorld")
 print("-----------------------------------")
-print("Hello\bWorld")
+print("Hello\tWorld")
+print("-----------------------------------")
+print("Hellow\bWorld")
+
+print("***********************************")
+age = 22
+score = 67.85
+print(age)
+print(score)
+
