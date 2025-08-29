f23332fc8a0f82e24dd71c2e80b30163b0245e2d
induwara20014 <lakshaninduwara067@gmail.com>
2025-08-29 23:41:33 +0530

    cost function and gradient medel

---
+++ code.pro.lk/Cost function and Gradient Model.py
@@ -0,0 +1,38 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+from Demos.print_desktop import dmsize
+
+#%%
+
+#%%
+# simple dataset
+x = np.array([1,2,3,4,5])
+y = np.array([5,8,11,14,17])
+#%%
+
+#%%
+#start with m = 0 and c = 0
+m = 0
+c = 0
+learning_rate = 0.01
+#%%
+
+for i in range(1,101):
+    y_predict = m * x + c
+    #calculate cost
+    cost = (1/n) * sum([value ** 2 for value in (y - y_predict)])
+
+    # plot after each iteration cost against m
+    plt.scatter(m , cost)
+
+    #calculate gradients
+    dm = -(2 / n) * sum(x * (y - y_predict))
+    dc = -(2 / n) * sum(y -y_predict])
+#%%
+   #update parameters
+   m = m - learning_rate * dm
+   c = c - learning_rate * dc
+#%%
+   print("m {}, c {},cost {} iteration {}".format(m,c,cost,i))
+   plt.show()
