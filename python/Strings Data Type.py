fb8bdf4dcc09bfdf59b6d2cbfd2c47b160ba606b
induwara20014 <lakshaninduwara067@gmail.com>
2025-08-30 22:00:07 +0530

    Strings Data Types

---
+++ python/Strings Data Type.py
@@ -0,0 +1,23 @@
+name = "CodePRO LK"
+
+print(name[0])
+print(name[-6])
+print(name[4:7])
+print(name[0:4])
+print(name[:10])
+print(name[4:])
+
+print("---------------------------------------------------")
+
+s1 = "Python"
+s2 = "Programming"
+s3 = 45
+
+print(s1 + " " + s2 + " " + str(s3))
+
+print("---------------------------------------------------")
+
+print("C" in name)
+print("PRO" in name)
+print("P" not in name)
+
