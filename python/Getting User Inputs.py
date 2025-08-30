8a43e9902ffd8e643a9a3d5c8aee58855fe2cee0
induwara20014 <lakshaninduwara067@gmail.com>
2025-08-30 21:43:35 +0530

    Getting user input

---
+++ python/Getting User Inputs.py
@@ -0,0 +1,21 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+score = float(input("Enter your score: "))
+
+print(f"Name is {name}")
+print(f"Age is {age}")
+print(f"Score is {score}")
+
+print(type(name))
+print(type(age))
+print(type(score))
+
+print("----------------------------------------------------")
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+sum  = num1 + num2
+print(f"Sum is: {sum}")
+
+
