d51aade4912c1c624491a775225727876737da8e
induwara20014 <lakshaninduwara067@gmail.com>
2025-08-31 15:00:21 +0530

    Strings methods

---
+++ python/Strings mehods.py
@@ -0,0 +1,38 @@
+name = "CodePRO LK"
+
+print(name)
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name.capitalize())
+
+print("-------------------------------------------")
+
+#find() and index()
+
+print(name.find('d'))
+print(name.find('z'))
+print(name.find('O',3))
+
+print(name.index('K'))
+print(name.index('O',3))
+
+print("-------------------------------------------")
+
+print(name.center(20, "-"))
+print(name.ljust(20,"-"))
+print(name.rjust(20, "-"))
+
+print("---------------------------------------------")
+
+name = "-----Induwara  lakshan-------"
+print(name)
+print(name.strip('-'))
+print(name.rstrip('-'))
+print(name.lstrip('-'))
+
+
+
+
+
+
