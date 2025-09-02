0e6596d867e53075a95a38a3c9d85e9beff4a3f3
induwara20014 <lakshaninduwara067@gmail.com>
2025-09-02 22:56:04 +0530

    List data type

---
+++ python/List data type.py
@@ -0,0 +1,12 @@
+lst1 = [20,30,10,60]
+lst2 = list((12,43,67,89))
+
+print(lst1)
+print(lst2)
+print(type(lst1))
+print(type(lst2))
+
+my_list = ['kamal',20,'kandy',False,20]
+print(my_list[-5])
+print(my_list[2:4])
+print(my_list[0:3])
